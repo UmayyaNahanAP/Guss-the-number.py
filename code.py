d697800116bbef7ab33d73b6fd55393fc428b82a
Umayya Nahan.A P <88381959+UmayyaNahanAP@users.noreply.github.com>
2022-09-07 09:22:35 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,18 @@
+#Guss the number-computer
+
+import random
+def guess(x):
+    random_number=random.randint(1,x)
+    guess=0
+    while guess !=random_number:
+        guess=int(input(f"Guess a number between 1 and {x} :"))
+        if guess<random_number:
+            print("Sorry,guess again,Too low")
+        elif guess>random_number:
+            print("Sorry,guess again,Too heigh")
+    print("Yya Congrats,you have guessed the number.")
+n=int(input("Enter the number up to : "))
+guess(n)
+
+
+
